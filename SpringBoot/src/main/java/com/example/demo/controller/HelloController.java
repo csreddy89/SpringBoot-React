63f7463eeb05a4d35c88efdf78a2d9fa9adb3463
csreddy89 <tseek2021@gmail.com>
2025-04-12 08:46:02 +0530

    Initial Commit

---
+++ SpringBoot/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,20 @@
+
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public Map<String, String> sayHello() {
+        return Map.of("message", "Hello from Spring Boot!");
+    }
+}
